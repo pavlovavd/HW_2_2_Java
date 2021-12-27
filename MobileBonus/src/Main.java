4eef88f6070de3924a3efb4cfcb02ca98d696832
Victoria Pavlova <pvd0510@mail.ru>
2021-12-27 19:33:01 +0300

    New commit for develop

--- MobileBonus/src/Main.java
+++ MobileBonus/src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int currentAccount = 100; // текущий счет (руб)
-        int refillBalance = 1000; // сумма (руб) пополнения баланса
+        int refillBalance = 100; // сумма (руб) пополнения баланса
         int moneyOfRefill = 100; // сумма (руб) за начисление 1 бонуса
 
         int finalAccount; // Итоговый счет
-        int bonus = 0;
+        int bonus;
 
         if (refillBalance < 1000) {
-            finalAccount = currentAccount + refillBalance;
-        }
-        else {
+            bonus = 0;
+        } else {
             bonus = refillBalance / moneyOfRefill;
-            finalAccount = currentAccount + refillBalance + bonus;
         }
+        finalAccount = currentAccount + refillBalance + bonus;
         System.out.println("Итоговый счет :" + finalAccount);
         System.out.println("Колличество бонусов:" + bonus);
     }
